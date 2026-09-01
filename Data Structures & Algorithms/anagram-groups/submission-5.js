d61c5b8305bc59b69c04ecefe6e97a28628908d5
Ryan <30806319+ryanrw@users.noreply.github.com>
2026-09-01 20:06:14 +0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const data = new Map()
+        strs.forEach((str) => {
+            const list = Array(26).fill(0)
+
+            for (const s of str) {
+                const index = s.charCodeAt(0) - "a".charCodeAt(0)
+                list[index] = list[index] + 1
+            }
+
+            const key = list.join()
+            if (data.has(key)) {
+                const value = data.get(key)
+                value.push(str)
+                data.set(key, value)
+            } else {
+                data.set(key, [str])
+            }
+        })
+        
+        const list = []
+        data.forEach((value, _) => {
+            list.push(value)
+        })
+
+        return list
+    }
+}
