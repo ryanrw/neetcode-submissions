229704d164b24c3be359f56eec0fcb33d9eda35f
Ryan <30806319+ryanrw@users.noreply.github.com>
2026-09-08 01:09:10 +0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -0,0 +1,70 @@
+class Solution {
+    // s convert to lowercase
+
+        // was saw?
+        // ^      ^
+        // i      j
+        // i is between a-z, 0-9 ok
+        // j is not alphanumerical, skip > j - 1
+
+        // was saw?
+        // i     j
+        // i is alphanumerical ok
+        // j is alphanumerical ok
+        // i == j ok i + 1, j - 1
+        
+        // was sxw?
+        //  i   j
+        // i is alphanumerical ok
+        // j is alphanumerical ok
+        // i !== j > false
+
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const lowercase = s.toLowerCase()
+
+        let i = 0
+        let j = lowercase.length - 1
+
+        while (i < j) {
+            console.log(`i: ${i}(${lowercase[i]}), j: ${j}(${lowercase[j]})`)
+            if (!this.isAlphanumerical(lowercase[i])) {
+                i++
+                continue
+            }
+
+            if (!this.isAlphanumerical(lowercase[j])) {
+                j--
+                continue
+            }
+
+            if (lowercase[i] !== lowercase[j]) {
+                return false
+            }
+
+            i++
+            j--
+        }
+        
+        return true
+    }
+
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isAlphanumerical(s) {
+        if (s.charCodeAt(0) >= "a".charCodeAt(0) && s.charCodeAt(0) <= "z".charCodeAt(0)) {
+            return true
+        }
+
+        if (s.charCodeAt(0) >= "0".charCodeAt(0) && s.charCodeAt(0) <= "9".charCodeAt(0)) {
+            return true
+        }
+
+        return false
+    }
+}
